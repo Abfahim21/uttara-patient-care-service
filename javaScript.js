34d3fa1ef7e53295d23365f6de1897b172d7cf42
Md. Abrar Fahim <abrarfahim21@gmail.com>
2023-12-16 04:55:40 +0600

    loading animation

---
+++ javaScript.js
@@ -0,0 +1,6 @@
+$(window).on("load", function () {
+  $(".loadding-page").delay(6000).fadeOut(200);
+  $(".cssload-box-loading").on("click", function () {
+    $(".cssload-box-loading").fadeOut(200);
+  });
+});
