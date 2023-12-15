b184f10972de6445f7eca2ab5390fe14559e62b3
Md. Abrar Fahim <abrarfahim21@gmail.com>
2023-12-16 05:00:21 +0600

    another loading animation

--- javaScript.js
+++ javaScript.js
@@ -1,6 +1,7 @@
-$(window).on("load", function () {
-  $(".loadding-page").delay(6000).fadeOut(200);
-  $(".cssload-box-loading").on("click", function () {
-    $(".cssload-box-loading").fadeOut(200);
+$(function () {
+  $(window).load(function () {
+    //on production change 3000 to 500
+    $(".pageloader").delay(3000).slideUp(800);
+    $("#loading-text").addClass("text-success").html("page loaded");
   });
 });
