9c368a867f3a5415c75c4ac96d57e045c01292ff
Md. Abrar Fahim <abrarfahim21@gmail.com>
2023-12-16 05:17:42 +0600

    fixed

--- javaScript.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-  $(window).load(function () {
-    //on production change 3000 to 500
-    $(".pageloader").delay(3000).slideUp(800);
-    $("#loading-text").addClass("text-success").html("page loaded");
-  });
-});
